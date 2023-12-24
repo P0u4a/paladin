63aa4fd30ed9096c8b2c84588c89f196c8af6dcd
P0u4a <pouyamiria@gmail.com>
2023-12-24 19:20:41 +1100

    Setup project

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,33 +1,42 @@
-import NextAuth, { type NextAuthOptions } from "next-auth";
-import CredentialsProvider from "next-auth/providers/credentials";
-import prisma from "@/lib/prisma";
-import { compare } from "bcrypt";
+import NextAuth, { Session, type NextAuthOptions } from 'next-auth';
+import CredentialsProvider from 'next-auth/providers/credentials';
+import prisma from '@/lib/prisma';
+import { compare } from 'bcrypt';
+import { User } from '@prisma/client';
 
 export const authOptions: NextAuthOptions = {
-  providers: [
-    CredentialsProvider({
-      credentials: {
-        email: { label: "Email", type: "email" },
-        password: { label: "Password", type: "password" }
-      },
-      async authorize(credentials) {
-        const { email, password } = credentials ?? {}
-        if (!email || !password) {
-          throw new Error("Missing username or password");
-        }
-        const user = await prisma.user.findUnique({
-          where: {
-            email,
-          },
-        });
-        // if user doesn't exist or password doesn't match
-        if (!user || !(await compare(password, user.password))) {
-          throw new Error("Invalid username or password");
-        }
-        return user;
-      },
-    }),
-  ],
+    providers: [
+        CredentialsProvider({
+            credentials: {
+                email: { label: 'Email', type: 'email' },
+                password: { label: 'Password', type: 'password' },
+            },
+            // @ts-ignore
+            async authorize(credentials) {
+                const { email, password } = credentials ?? {};
+                if (!email || !password) {
+                    throw new Error('Missing username or password');
+                }
+                const user = await prisma.user.findUnique({
+                    where: {
+                        email,
+                    },
+                });
+                // if user doesn't exist or password doesn't match
+                if (!user || !(await compare(password, user.password))) {
+                    throw new Error('Invalid username or password');
+                }
+                return user;
+            },
+        }),
+    ],
+    callbacks: {
+        session: async (session: Session, user: User) => {
+            // Add the user's ID to the session
+            session.user.id = user.id;
+            return session;
+        },
+    },
 };
 
 const handler = NextAuth(authOptions);
---
+++ components/project-card.tsx
@@ -0,0 +1,19 @@
+import Link from 'next/link';
+import { Card, CardTitle, CardHeader } from './ui/card';
+
+type CardProps = {
+    id: number;
+    name: string;
+};
+
+export default function ProjectCard({ name, id }: CardProps) {
+    return (
+        <Link href={`/projects/${id}`} className="max-w-xs">
+            <Card>
+                <CardHeader>
+                    <CardTitle>{name}</CardTitle>
+                </CardHeader>
+            </Card>
+        </Link>
+    );
+}
